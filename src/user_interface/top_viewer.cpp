4ef9b8dcd741168e6f89a6c959ba0927e530cdd7
sangwon lee <leeh8911@gmail.com>
2023-02-27 05:57:17 +0900

    update top-view class

---
+++ src/user_interface/top_viewer.cpp
@@ -0,0 +1,46 @@
+﻿/// @file top_viewer.cpp
+/// @author sangwon lee (leeh8911@gmail.com)
+/// @brief
+/// @version 0.1
+/// @date 2023-02-27
+///
+/// @copyright Copyright (c) 2023
+///
+///
+
+#include "src/user_interface/top_viewer.h"
+
+#include <imgui.h>
+
+#include "src/data_structure/frame.h"
+#include "src/utils/log.h"
+
+namespace ad_framework::ui
+{
+
+TopViewer::TopViewer(std::string title, ImVec2 size, ImVec2 pos)
+    : UserInterface(title, size, pos)
+{
+}
+
+void TopViewer::operator()()
+{
+    ImGui::Begin(Title().c_str());
+    ImGui::End();
+}
+
+std::any TopViewer::FrameSender::operator()(const std::any& src)
+{
+    if (!src.has_value())
+    {
+        LOG(log::LogLevel::kDebug);
+    }
+    return frame_;
+}
+
+void TopViewer::FrameSender::SetFrame(const data_structure::Frame& frame)
+{
+    frame_ = frame;
+}
+
+}  // namespace ad_framework::ui
---
+++ src/user_interface/top_viewer.h
@@ -0,0 +1,46 @@
+﻿/// @file top_viewer.h
+/// @author sangwon lee (leeh8911@gmail.com)
+/// @brief
+/// @version 0.1
+/// @date 2023-02-27
+///
+/// @copyright Copyright (c) 2023
+///
+///
+
+#ifndef SRC_USER_INTERFACE_TOP_VIEWER_H_
+#define SRC_USER_INTERFACE_TOP_VIEWER_H_
+
+#include "src/data_structure/frame.h"
+#include "src/user_interface/ui.h"
+
+namespace ad_framework::ui
+{
+
+class TopViewer : public UserInterface
+{
+ public:
+    TopViewer(std::string title, ImVec2 size, ImVec2 pos);
+    void operator()() override;
+
+    class FrameSender;
+
+ private:
+};
+
+class TopViewer::FrameSender : public callback::Callback
+{
+ public:
+    FrameSender() = default;
+    std::any operator()(const std::any& src) override;
+
+    void SetFrame(const data_structure::Frame& frame);
+
+ private:
+    data_structure::Frame frame_{};
+    std::string title_{};
+};
+
+}  // namespace ad_framework::ui
+
+#endif  // SRC_USER_INTERFACE_TOP_VIEWER_H_
